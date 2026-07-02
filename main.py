b7d6c3d9840194e95446d4b5f939ae77e957023c
Dalton <dalton.wright033@gmail.com>
2026-07-02 10:27:53 -0500

    Made function input more generic so as to be able to input any file path for parsing

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
+# import re
 
-file = "sample.txt"
 alert_word_login = "failed password"
 # TODO: add function that will update the final count according to the activity observed. Perhaps a new function for each?
-# TODO: modify parser to extract information like:
+# TODO: modify parser to extract information like: (Use RegEx)
 #Time: 08:22:11
 #Host: webserver01
 #Service: sshd
 #IP: 203.0.113.55
 
-# Looks for specific search word in log file and counts instances of that word.
-def failed__login_attempts():
+# Looks for failed login attempts in log file and counts instances.
+def failed__login_attempts(file):
     with open(file, "r") as f:
             count = 0
             for line in f:
                     if alert_word_login.lower() in line.lower():
                         count += 1
-                        print(f"Found {alert_word_login.lower()} in {line}")
+                        print(f"Found '{alert_word_login.lower()}' in {line}")
             print(f"{count} failed login attempts.")
 
-failed__login_attempts()
+failed__login_attempts("sample.txt")
