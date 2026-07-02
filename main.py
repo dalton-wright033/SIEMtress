2551db6b0d45a0516d613f77009a02202efee324
Dalton <dalton.wright033@gmail.com>
2026-07-01 22:15:55 -0500

    Updated alert word variable to reflect specific purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 file = "sample.txt"
-alert_word = "failed password"
+alert_word_login = "failed password"
 # TODO: add function that will update the final count according to the activity observed. Perhaps a new function for each?
 # TODO: Make alert_word more specific so false flags like "backup failed" causes alert.
 # TODO: modify parser to extract information like:
@@ -14,9 +14,9 @@ def failed__login_attempts():
     with open(file, "r") as f:
             count = 0
             for line in f:
-                    if alert_word.lower() in line.lower():
+                    if alert_word_login.lower() in line.lower():
                         count += 1
-                        print(f"Found {alert_word.lower()} in {line}")
+                        print(f"Found {alert_word_login.lower()} in {line}")
             print(f"{count} failed login attempts.")
 
 failed__login_attempts()
