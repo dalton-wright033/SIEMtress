cfe3971eda8617046db2ef9987ce3994db8f40e4
Dalton <dalton.wright033@gmail.com>
2026-07-01 22:14:48 -0500

    Made trigger word of failed login attempts more specifc.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 file = "sample.txt"
-alert_word = "failed"
+alert_word = "failed password"
 # TODO: add function that will update the final count according to the activity observed. Perhaps a new function for each?
 # TODO: Make alert_word more specific so false flags like "backup failed" causes alert.
 # TODO: modify parser to extract information like:
@@ -10,10 +10,13 @@ alert_word = "failed"
 #IP: 203.0.113.55
 
 # Looks for specific search word in log file and counts instances of that word.
-with open(file, "r") as f:
-        count = 0
-        for line in f:
-                if alert_word.lower() in line.lower():
-                    count += 1
-                    print(f"Found {alert_word.lower()} in {line}")
-        print(f"{count} failed login attempts.")
+def failed__login_attempts():
+    with open(file, "r") as f:
+            count = 0
+            for line in f:
+                    if alert_word.lower() in line.lower():
+                        count += 1
+                        print(f"Found {alert_word.lower()} in {line}")
+            print(f"{count} failed login attempts.")
+
+failed__login_attempts()
