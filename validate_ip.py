124bc9e0f5feafded31df06ccb0a87c4496d9d87
Dalton <dalton.wright033@gmail.com>
2026-08-13 00:10:57 -0500

    Adding flesh to ip validator. Still need to fix some bugs.

--- validate_ip.py
+++ validate_ip.py
@@ -1,1 +1,11 @@
+import extract_event as ee
+import ipaddress
+
+
 def validate_ip(ip):
+    try:
+        valid_IP = ipaddress.ip_address(ee.extracted_data["ipv4"])
+    except ValueError:
+        print(f"Found invalid IP on {ee.line}: {ee.extracted_data["ipv4"]}\n")
+        return None
+    return valid_IP
